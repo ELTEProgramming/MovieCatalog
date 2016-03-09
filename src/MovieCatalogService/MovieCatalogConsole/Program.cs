13fdc503375444d1a53703bc97a2f0b0350627a3
Kornél Bálint Kátai <kornel.katai@dealogic.com>
2016-03-09 17:38:55 +0100

    Initial commit.

---
+++ src/MovieCatalogService/MovieCatalogConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using MovieCatalogService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new OmdbMovieSource();
+            var movie = source.GetMovieById("tt0371746");
+
+            Console.WriteLine("{0}: {1}", movie.Id, movie.Title);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ src/MovieCatalogService/MovieCatalogService/UriHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MovieCatalogService
+{
+    public static class UriHelpers
+    {
+        public static Uri AppendQueryArgument(this Uri baseUri, KeyValuePair<string, string> queryArgument)
+        {
+            var uriBuilder = new UriBuilder(baseUri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query[queryArgument.Key] = queryArgument.Value;
+            uriBuilder.Query = query.ToString();
+
+            return uriBuilder.Uri;
+        }
+    }
+}
